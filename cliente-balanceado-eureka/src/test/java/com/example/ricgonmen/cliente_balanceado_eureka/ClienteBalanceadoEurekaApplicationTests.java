97e1db63287339e4683b44c1d1901048376149ba
Ricardo <ricgonmen@gmail.com>
2021-08-08 10:13:59 +0200

    L6

---
+++ cliente-balanceado-eureka/src/test/java/com/example/ricgonmen/cliente_balanceado_eureka/ClienteBalanceadoEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ricgonmen.cliente_balanceado_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteBalanceadoEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
